221c2e17ac7529e82edefef4fe73aef34fe46e7c
Leo <liyung.chen@tum.de>
2025-03-19 23:15:19 +0100

    refactor test code

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: 'jsdom',
-        setupFiles: ['./src/test/setup.ts'],
+        setupFiles: ['./setup.ts'],
         globals: true
     }
 });
